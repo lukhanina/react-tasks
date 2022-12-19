2a56a81d9072b00374dac4e12114d678a59d0805
lukhanina <111492804+lukhanina@users.noreply.github.com>
2022-12-19 11:37:54 +0100

    fix: checkbox

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { byEnter } from '../../store/profileReducer';
 
 export default function Profile() {
   const dispatch = useDispatch();
-    return (
-      <div className='absolute'>
-        <div className="profile">
-          <Link to={'/'}>
-            <button className="close-profile">&times;
-            </button>
-          </Link>
-          <div className='profile__checkbox'>
-            <input type="checkbox" id="byEnter" name="onEnter" value="byEnter" onChange={(event) => {dispatch(byEnter(event.target.checked))}} defaultChecked/>
-            <label htmlFor="byEnter">Send by Enter</label>
-          </div>
+  const byEnterValue = useSelector((state) => state.profile.byEnter);
+  return (
+    <div className='absolute'>
+      <div className="profile">
+        <Link to={'/'}>
+          <button className="close-profile">&times;
+          </button>
+        </Link>
+        <div className='profile__checkbox'>
+          <input type="checkbox" id="byEnter" name="onEnter" value="byEnter" onChange={(event) => {dispatch(byEnter(event.target.checked))}} defaultChecked={byEnterValue}/>
+          <label htmlFor="byEnter">Send by Enter</label>
         </div>
       </div>
-    )
+    </div>
+  )
 }
