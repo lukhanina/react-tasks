766d67be0dcf5c1782cfab5b93d300dd6c7b67cf
lukhanina <111492804+lukhanina@users.noreply.github.com>
2022-12-22 17:38:37 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
                 options: {
-                    plugins: [
-                        ["@babel/plugin-proposal-class-properties", { "loose": true }],
-                        ["@babel/plugin-proposal-private-methods", { "loose": true }],
-                        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-                    ]
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
                 }
             },
             {
